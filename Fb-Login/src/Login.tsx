9435c306828be51fdb0f25e29763c622a93b52fe
RandiyaFer <pamindurandiya34@gmail.com>
2024-02-18 12:32:00 +0530

    Add responsive

--- Fb-Login/src/Login.tsx
+++ Fb-Login/src/Login.tsx
@@ -5,22 +5,38 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js"
 function Login() {
     return (
         <>
-            <div className="content">
-                <div className="flex-div">
-                    <div className="name-content">
-                        <h1 className="logo">Facebook</h1>
-                        <p>Connect with friends and the world around you on Facebook.</p>
+        
+        <div className="content">
+            <div className="flex-div">
+            <div className='container'>
+                <div className='row'>           
+                    <div className='col-lg-6 col-sm-12'>
+                        <div className="name-content">
+                            <h1 className="logo">Facebook</h1>
+                            <p>Facebook helps you connect and share with the people in your life.</p>
+                        </div>
                     </div>
-                    <form>
-                        <input type="text" placeholder="Email or Phone Number" required />
-                        <input type="password" placeholder="Password" required />
-                        <button className="login">Log In</button>
-                        <a href="#">Forgot Password ?</a>
-                        <hr />
-                        <button className="create-account">Create New Account</button>
-                    </form>
+                    <div className='col-lg-6 col-sm-12'>
+                        <form>
+                            <input type="email" className="form-control" placeholder="Email or Phone Number"></input>
+                            <input type="password" className="form-control" placeholder="Password"></input>
+                            <button className="login">Log In</button>
+                            <div className='ar'>
+                                <a href="#">Forgot Password ?</a>
+                            </div>
+
+                            <hr />
+                            <button className="create-account">Create New Account</button>
+                            <div className='pr2 mt-2'>
+                                <a href=""> create page</a>
+                                for a celebrity, brand or business.
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
+        </div>
+        </div>
         </>
     );
 };
